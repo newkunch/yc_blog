c0d4b823c612ff54d2407ab50ad839b7ddb6c28a
newkunch <newkunch@gmail.com>
2020-09-13 20:11:54 +0800

    博客工程的初始化创建

---
+++ C81-lkblog/src/main/java/com/yc/C81lkblog/C81LkblogApplication.java
@@ -0,0 +1,13 @@
+package com.yc.C81lkblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C81LkblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C81LkblogApplication.class, args);
+	}
+
+}
---
+++ C81-lkblog/src/test/java/com/yc/C81lkblog/C81LkblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.C81lkblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C81LkblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
